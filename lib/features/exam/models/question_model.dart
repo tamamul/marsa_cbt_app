654e66cca7bf00b23b7d5791249c9bab8d63dad6
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-06 23:42:38 +0700

    Create lib/features/exam/models/question_model.dart

---
+++ lib/features/exam/models/question_model.dart
@@ -0,0 +1,46 @@
+class OptionModel {
+  final int    id;
+  final String label;
+  final String content;
+
+  const OptionModel({
+    required this.id,
+    required this.label,
+    required this.content,
+  });
+
+  factory OptionModel.fromJson(Map<String, dynamic> json) => OptionModel(
+    id:      json['id'] as int,
+    label:   json['label'] as String,
+    content: json['content'] as String,
+  );
+}
+
+class QuestionModel {
+  final int              id;
+  final String           content;
+  final String           type;
+  final String?          imagePath;
+  final double           points;
+  final List<OptionModel> options;
+
+  const QuestionModel({
+    required this.id,
+    required this.content,
+    required this.type,
+    this.imagePath,
+    required this.points,
+    required this.options,
+  });
+
+  factory QuestionModel.fromJson(Map<String, dynamic> json) => QuestionModel(
+    id:        json['id'] as int,
+    content:   json['content'] as String,
+    type:      json['type'] as String,
+    imagePath: json['image_path'] as String?,
+    points:    (json['points'] ?? 1).toDouble(),
+    options:   (json['options'] as List)
+        .map((o) => OptionModel.fromJson(o as Map<String, dynamic>))
+        .toList(),
+  );
+}
