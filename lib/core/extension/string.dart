573b30803490539276200935f5401b42054f6f1c
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-07 11:35:10 +0700

    Delete string.dart

--- lib/core/extension/string.dart
+++
@@ -1,7 +0,0 @@
-extension StringExtension on String {
-  bool isValidEmail() {
-    return RegExp(
-      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-    ).hasMatch(this);
-  }
-}
