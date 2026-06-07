31c5cbcc781fa78b320fc267c3361f4baebffe8c
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-07 11:34:24 +0700

    Delete logger.dart

--- lib/core/logger/logger.dart
+++
@@ -1,9 +0,0 @@
-import 'package:loggy/loggy.dart' as loggy;
-
-typedef LoggerFunction =
-    void Function(String message, [dynamic error, StackTrace? stackTrace]);
-
-final LoggerFunction logDebug = loggy.logDebug;
-final LoggerFunction logInfo = loggy.logInfo;
-final LoggerFunction logWarning = loggy.logWarning;
-final LoggerFunction logError = loggy.logError;
